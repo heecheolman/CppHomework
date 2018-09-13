6a448948b82c126fea0670396ed0ac81c740812e
heecheolman <msn9975@gmail.com>
2018-09-13 14:23:01 +0900

    readme 파일 추가

---
+++ Homework2/Homework2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Homework2
+//
+//  Created by HeeCheol Kim on 2018. 9. 13..
+//  Copyright © 2018년 HeeCheol Kim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
